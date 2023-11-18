a4a7da1382e74cca91b53f17bf49a678d5fe8a32
MSulthonA <muhammadsulthonauliya313@gmail.com>
2023-11-18 20:06:51 +0700

    first commit

---
+++ TempConvVBClient/TempConvVBClient/Form1.Designer.vb
@@ -0,0 +1,257 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ctof_button = New System.Windows.Forms.Button()
+        Me.input_textbox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.hasil_label = New System.Windows.Forms.TextBox()
+        Me.ftoc_button = New System.Windows.Forms.Button()
+        Me.ctor_button = New System.Windows.Forms.Button()
+        Me.ftor_button = New System.Windows.Forms.Button()
+        Me.rtoc_button = New System.Windows.Forms.Button()
+        Me.rtof_button = New System.Windows.Forms.Button()
+        Me.ktoc_button = New System.Windows.Forms.Button()
+        Me.ktof_button = New System.Windows.Forms.Button()
+        Me.ctok_button = New System.Windows.Forms.Button()
+        Me.ftok_button = New System.Windows.Forms.Button()
+        Me.rtok_button = New System.Windows.Forms.Button()
+        Me.ktor_button = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ctof_button
+        '
+        Me.ctof_button.Location = New System.Drawing.Point(156, 185)
+        Me.ctof_button.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ctof_button.Name = "ctof_button"
+        Me.ctof_button.Size = New System.Drawing.Size(60, 48)
+        Me.ctof_button.TabIndex = 0
+        Me.ctof_button.Text = "C to F"
+        Me.ctof_button.UseVisualStyleBackColor = True
+        '
+        'input_textbox
+        '
+        Me.input_textbox.Location = New System.Drawing.Point(266, 143)
+        Me.input_textbox.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.input_textbox.Name = "input_textbox"
+        Me.input_textbox.Size = New System.Drawing.Size(142, 27)
+        Me.input_textbox.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(26, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(547, 46)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Temperature Converter VB Client"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(183, 143)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Input"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(170, 373)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 38)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Hasil"
+        '
+        'hasil_label
+        '
+        Me.hasil_label.Font = New System.Drawing.Font("Segoe UI", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.hasil_label.Location = New System.Drawing.Point(266, 368)
+        Me.hasil_label.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.hasil_label.Name = "hasil_label"
+        Me.hasil_label.Size = New System.Drawing.Size(142, 43)
+        Me.hasil_label.TabIndex = 5
+        '
+        'ftoc_button
+        '
+        Me.ftoc_button.Location = New System.Drawing.Point(238, 185)
+        Me.ftoc_button.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ftoc_button.Name = "ftoc_button"
+        Me.ftoc_button.Size = New System.Drawing.Size(60, 48)
+        Me.ftoc_button.TabIndex = 6
+        Me.ftoc_button.Text = "F to C"
+        Me.ftoc_button.UseVisualStyleBackColor = True
+        '
+        'ctor_button
+        '
+        Me.ctor_button.Location = New System.Drawing.Point(156, 241)
+        Me.ctor_button.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ctor_button.Name = "ctor_button"
+        Me.ctor_button.Size = New System.Drawing.Size(60, 48)
+        Me.ctor_button.TabIndex = 7
+        Me.ctor_button.Text = "C to R"
+        Me.ctor_button.UseVisualStyleBackColor = True
+        '
+        'ftor_button
+        '
+        Me.ftor_button.Location = New System.Drawing.Point(238, 241)
+        Me.ftor_button.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ftor_button.Name = "ftor_button"
+        Me.ftor_button.Size = New System.Drawing.Size(60, 48)
+        Me.ftor_button.TabIndex = 8
+        Me.ftor_button.Text = "F to R"
+        Me.ftor_button.UseVisualStyleBackColor = True
+        '
+        'rtoc_button
+        '
+        Me.rtoc_button.Location = New System.Drawing.Point(316, 185)
+        Me.rtoc_button.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.rtoc_button.Name = "rtoc_button"
+        Me.rtoc_button.Size = New System.Drawing.Size(60, 48)
+        Me.rtoc_button.TabIndex = 9
+        Me.rtoc_button.Text = "R to C"
+        Me.rtoc_button.UseVisualStyleBackColor = True
+        '
+        'rtof_button
+        '
+        Me.rtof_button.Location = New System.Drawing.Point(316, 241)
+        Me.rtof_button.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.rtof_button.Name = "rtof_button"
+        Me.rtof_button.Size = New System.Drawing.Size(60, 48)
+        Me.rtof_button.TabIndex = 10
+        Me.rtof_button.Text = "R to F"
+        Me.rtof_button.UseVisualStyleBackColor = True
+        '
+        'ktoc_button
+        '
+        Me.ktoc_button.Location = New System.Drawing.Point(396, 185)
+        Me.ktoc_button.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ktoc_button.Name = "ktoc_button"
+        Me.ktoc_button.Size = New System.Drawing.Size(60, 48)
+        Me.ktoc_button.TabIndex = 11
+        Me.ktoc_button.Text = "K to C"
+        Me.ktoc_button.UseVisualStyleBackColor = True
+        '
+        'ktof_button
+        '
+        Me.ktof_button.Location = New System.Drawing.Point(396, 241)
+        Me.ktof_button.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ktof_button.Name = "ktof_button"
+        Me.ktof_button.Size = New System.Drawing.Size(60, 48)
+        Me.ktof_button.TabIndex = 12
+        Me.ktof_button.Text = "K to F"
+        Me.ktof_button.UseVisualStyleBackColor = True
+        '
+        'ctok_button
+        '
+        Me.ctok_button.Location = New System.Drawing.Point(156, 297)
+        Me.ctok_button.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ctok_button.Name = "ctok_button"
+        Me.ctok_button.Size = New System.Drawing.Size(60, 48)
+        Me.ctok_button.TabIndex = 13
+        Me.ctok_button.Text = "C to K"
+        Me.ctok_button.UseVisualStyleBackColor = True
+        '
+        'ftok_button
+        '
+        Me.ftok_button.Location = New System.Drawing.Point(238, 297)
+        Me.ftok_button.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ftok_button.Name = "ftok_button"
+        Me.ftok_button.Size = New System.Drawing.Size(60, 48)
+        Me.ftok_button.TabIndex = 14
+        Me.ftok_button.Text = "F to K"
+        Me.ftok_button.UseVisualStyleBackColor = True
+        '
+        'rtok_button
+        '
+        Me.rtok_button.Location = New System.Drawing.Point(316, 297)
+        Me.rtok_button.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.rtok_button.Name = "rtok_button"
+        Me.rtok_button.Size = New System.Drawing.Size(60, 48)
+        Me.rtok_button.TabIndex = 15
+        Me.rtok_button.Text = "R to K"
+        Me.rtok_button.UseVisualStyleBackColor = True
+        '
+        'ktor_button
+        '
+        Me.ktor_button.Location = New System.Drawing.Point(396, 297)
+        Me.ktor_button.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ktor_button.Name = "ktor_button"
+        Me.ktor_button.Size = New System.Drawing.Size(60, 48)
+        Me.ktor_button.TabIndex = 16
+        Me.ktor_button.Text = "K to R"
+        Me.ktor_button.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(590, 485)
+        Me.Controls.Add(Me.ktor_button)
+        Me.Controls.Add(Me.rtok_button)
+        Me.Controls.Add(Me.ftok_button)
+        Me.Controls.Add(Me.ctok_button)
+        Me.Controls.Add(Me.ktof_button)
+        Me.Controls.Add(Me.ktoc_button)
+        Me.Controls.Add(Me.rtof_button)
+        Me.Controls.Add(Me.rtoc_button)
+        Me.Controls.Add(Me.ftor_button)
+        Me.Controls.Add(Me.ctor_button)
+        Me.Controls.Add(Me.ftoc_button)
+        Me.Controls.Add(Me.hasil_label)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.input_textbox)
+        Me.Controls.Add(Me.ctof_button)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ctof_button As Button
+    Friend WithEvents input_textbox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents hasil_label As TextBox
+    Friend WithEvents ftoc_button As Button
+    Friend WithEvents ctor_button As Button
+    Friend WithEvents ftor_button As Button
+    Friend WithEvents rtoc_button As Button
+    Friend WithEvents rtof_button As Button
+    Friend WithEvents ktoc_button As Button
+    Friend WithEvents ktof_button As Button
+    Friend WithEvents ctok_button As Button
+    Friend WithEvents ftok_button As Button
+    Friend WithEvents rtok_button As Button
+    Friend WithEvents ktor_button As Button
+End Class
---
+++ TempConvVBClient/TempConvVBClient/Form1.vb
@@ -0,0 +1,98 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports TemperaturConverter
+
+Public Class Form1
+    Private Sub ctof_button_Click(sender As Object, e As EventArgs) Handles ctof_button.Click
+        Dim objConv As New TemperaturConverter.Class1
+        Dim c As Double
+        c = CDbl(input_textbox.Text)
+        hasil_label.Text = objConv.cToF(c)
+    End Sub
+
+    Private Sub ctor_button_Click(sender As Object, e As EventArgs) Handles ctor_button.Click
+        Dim objConv As New TemperaturConverter.Class1
+        Dim c As Double
+        c = CDbl(input_textbox.Text)
+        hasil_label.Text = objConv.cToR(c)
+    End Sub
+
+    Private Sub ctok_button_Click(sender As Object, e As EventArgs) Handles ctok_button.Click
+        Dim objConv As New TemperaturConverter.Class1
+        Dim c As Double
+        c = CDbl(input_textbox.Text)
+        hasil_label.Text = objConv.cToK(c)
+    End Sub
+
+
+
+
+    Private Sub ftoc_button_Click(sender As Object, e As EventArgs) Handles ftoc_button.Click
+        Dim objConv As New TemperaturConverter.Class1
+        Dim f As Double
+        f = CDbl(input_textbox.Text)
+        hasil_label.Text = objConv.fToC(f)
+    End Sub
+
+    Private Sub ftor_button_Click(sender As Object, e As EventArgs) Handles ftor_button.Click
+        Dim objConv As New TemperaturConverter.Class1
+        Dim f As Double
+        f = CDbl(input_textbox.Text)
+        hasil_label.Text = objConv.fToR(f)
+    End Sub
+
+    Private Sub ftok_button_Click(sender As Object, e As EventArgs) Handles ftok_button.Click
+        Dim objConv As New TemperaturConverter.Class1
+        Dim f As Double
+        f = CDbl(input_textbox.Text)
+        hasil_label.Text = objConv.fToK(f)
+    End Sub
+
+
+
+    Private Sub rtoc_button_Click(sender As Object, e As EventArgs) Handles rtoc_button.Click
+        Dim objConv As New TemperaturConverter.Class1
+        Dim r As Double
+        r = CDbl(input_textbox.Text)
+        hasil_label.Text = objConv.rToC(r)
+    End Sub
+
+    Private Sub rtof_button_Click(sender As Object, e As EventArgs) Handles rtof_button.Click
+        Dim objConv As New TemperaturConverter.Class1
+        Dim r As Double
+        r = CDbl(input_textbox.Text)
+        hasil_label.Text = objConv.rToF(r)
+    End Sub
+
+    Private Sub rtok_button_Click(sender As Object, e As EventArgs) Handles rtok_button.Click
+        Dim objConv As New TemperaturConverter.Class1
+        Dim r As Double
+        r = CDbl(input_textbox.Text)
+        hasil_label.Text = objConv.rToK(r)
+    End Sub
+
+
+
+
+    Private Sub ktoc_button_Click(sender As Object, e As EventArgs) Handles ktoc_button.Click
+        Dim objConv As New TemperaturConverter.Class1
+        Dim k As Double
+        k = CDbl(input_textbox.Text)
+        hasil_label.Text = objConv.kToC(k)
+    End Sub
+
+    Private Sub ktof_button_Click(sender As Object, e As EventArgs) Handles ktof_button.Click
+        Dim objConv As New TemperaturConverter.Class1
+        Dim k As Double
+        k = CDbl(input_textbox.Text)
+        hasil_label.Text = objConv.kToF(k)
+    End Sub
+
+    Private Sub ktor_button_Click(sender As Object, e As EventArgs) Handles ktor_button.Click
+        Dim objConv As New TemperaturConverter.Class1
+        Dim k As Double
+        k = CDbl(input_textbox.Text)
+        hasil_label.Text = objConv.kToR(k)
+    End Sub
+
+
+End Class
